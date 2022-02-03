dbc1e093dd2ee71d3af16e65c13a32f0bb76d75c
Fox-IceWings <isaaccr@protonmail.com>
2022-02-03 02:43:41 -0800

    Update
    Moved DB operations to another file
    Now using ASYNC IO
    Changed how station info is stored

---
+++ database.py
@@ -0,0 +1,127 @@
+from array import array
+from distutils.log import error
+from logging import exception
+import selectors
+import motor.motor_asyncio
+import asyncio
+from os import path
+import json     
+
+conn_str = ""
+# set a 5-second connection timeout
+client = motor.motor_asyncio.AsyncIOMotorClient(conn_str, serverSelectionTimeoutMS=5000)
+
+
+try: 
+    f = open("arrguments.json")
+    arrguments = json.load(f)
+    conn_str = arrguments["dataBase"]
+
+except:
+    print("FAILED TO LOAD ARGUMENTS")
+
+finally:
+    f.close()
+
+
+#write new station to database
+async def add_new_station(control, printReport, save, raw): #see note 1
+
+    
+    #see what type we are writing SITE/NAVAID/FIR/CAMERA/UNKOWEN
+    match raw[control[0]]["properties"]["productName"]:
+        #write as site
+        case "SITE":
+            #try to write to mongoDB
+            try:
+                #JSON to be written  
+                client.WX["Station Information"].insert_one({
+                    "_id": raw[control[0]]["properties"]["groupIdentifier"],
+                    "product": raw[control[0]]["properties"]["productName"],
+                    "type": raw[control[0]]["properties"]["groupType"],
+                    "use": {
+                        "useStation": control[1],
+                        "rapid": control[2],
+                        "print": {
+                            "metar": printReport[0],
+                            "taf": printReport[1],
+                            "notam": printReport[2]
+                        },
+                        "store": {
+                            "metar": save[0],
+                            "taf": save[1],
+                            "notam": save[2]
+                        }
+                    },
+                    "coordinates": raw[control[0]]["geometry"]["coordinates"],
+                    "radius": control[3]
+                    
+                    })
+
+            #failed to write to mongo :(
+            except Exception:
+                print("DATABASE WRITE ERROR")
+
+        case "FIR":
+            #try to write to mongoDB
+            try:
+                #JSON to be written
+               
+                client.WX["Station Information"].insert_one({
+                    "_id": raw[control[0]]["properties"]["groupIdentifier"],
+                    "product": raw[control[0]]["properties"]["productName"],
+                    "type": raw[control[0]]["geometry"]["groupType"],
+                    "use": {
+                        "useStation": control[1],
+                        "rapid": control[2],
+                        "print": {
+                            "sigmet" : False,
+                            "airmet" : False,
+                            "pirep" : False,
+                            "sigmet" : False,
+                            "BCVFR" : False,
+                            "FDL": {
+                                "YVR": True
+                            },
+                            "FDH": {
+                                "YVR": True
+                            }
+                        },
+                        "store": {
+                            "print": {
+                                "sigmet" : False,
+                                "airmet" : False,
+                                "pirep" : False,
+                                "sigmet" : False,
+                                "BCVFR" : False,
+                                "FDL": {
+                                    "YVR": True
+                                },
+                                "FDH": {
+                                    "YVR": True
+                                },
+                            }
+                        }
+                    },
+
+                    "coordinates": raw[control[0]]["geometry"]["coordinates"],
+                    "radius": control[3]
+                    
+                    })
+            
+
+            #failed to write to mongo :(
+            except Exception as e:
+                print("DATABASE WRITE ERROR")
+                print(e)
+
+
+# NOTE 1: arrguments are
+#
+#     selector: what the user chose during select if there were multiple stations by defult 
+#     this goes to 1 (note in future the number shown to user may not be the one repersentitive 
+#     of item chosen)
+#
+#     data: reffers to the options the user needs to input to write
+#
+#     raw: refers to the raw data supplied by NAV Canada
--- scraper.py
+++ scraper.py
@@ -1,54 +1,33 @@
 #from email.policy import default
 from fileinput import close
+from logging import exception
 import requests
 #from bs4 import BeautifulSoup
 import re
 import json
 from requests.api import request
+import os
 from os import path
+import pymongo
+import database
+import asyncio
 
+clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 
 staionInformationURL    = "https://plan.navcanada.ca/weather/api/search/en?include_polygons=true&filter[value]="
 wxDataURL               = ""
 
-localDatabaseFile = "configure.json"
-localDatabase = ""
+stationControl = []
+stationProducts = {
+    "stationPrint" : [],
+    "stationSave"  : []
 
+}
 #Obtain new staion information to write to local database
 def add_to_database(): 
 
-    #see if local database file exsists
-    if path.isfile(localDatabaseFile) is False:
-        print("Local Database Missing!!!")
-        return
-
-    desiredNewStaion = 0 #set variable and defult to 0
+    stationControl.append(0) #set variable and defult to 0
     newStation = ""
-
-    #new staion template
-    newStationJSON = {
-            "use"   : {
-                "useStation" : False,
-                "rapid"     : False,
-                "print"     : {
-                    "metar"     : False,
-                    "taf"       : False,
-                    "notam"     : False
-                },
-                "store"     : {
-                    "metar"     : False,
-                    "taf"       : False,
-                    "notam"     : False
-                }
-            },
-            "type"  : "",
-            "coordinates": ["-000.000", "00.000"],
-            "hasMetar" : False,
-            "hasTaf"   : False,
-            "radius"   : 0
-
-        }
-
 
     #New staion select loop
     while True:
@@ -58,6 +37,7 @@ def add_to_database():
 
         newStationData = requests.get(staionInformationURL+newStation).json()["data"] #Query Nav Canada using user input and save to variable
         newStationDataLength = (len(newStationData)) #get json array length to know how many results were returned
+        
         match newStationDataLength:
         
             case newStationDataLength if newStationDataLength > int(1): #see if API returned multiple items
@@ -76,7 +56,23 @@ def add_to_database():
                         print(nsqrOption + " - " + newStaionQueryResult["properties"]["productName"]) #add relevent data
 
                 print() #add blank line to look nice
-                desiredNewStaion = input() #override defult
+                
+                while True: #validate input
+                    try: #enterd a valid input
+                        stationControl[0] = int(input("Please pick from list: "))
+                        if stationControl[0] < newStationDataLength: #yes the < is correct no suposed to be <=
+                            break
+
+                        else:#did not pick form list
+                            print("Please pick from list")
+                            print()
+
+                    except: #input wasnt a number
+                        print("Please enter a number: ")
+                        print()
+
+
+
                 break #break out of new staion select loop set up loop
 
             case 1: #see if api returned 1
@@ -87,96 +83,96 @@ def add_to_database():
 
             case _: #i dont know what happend
                 print("Unexspected Error") #tell user err and ask again
-
-
-    #see if staion is already in database
-    f = open(localDatabaseFile)
-    localDatabase = json.load(f) #load in local dataBase
-    f.close()       
+      
 
     while True:
 
-        #alert user that staion is already in database
-        if newStationData[int(desiredNewStaion)]["properties"]["groupIdentifier"] in localDatabase["stationData"]:
-            print("Already in data base.")
-            break # exit loop
+        clearConsole()
+        print(newStationData[stationControl[0]]["properties"]["displayName"])
+        setUpQuestions = ["METAR","TAF","NOTAM"]
+        setUpPrintStore = ["Print","Save"]
+
+        #enable staion
+        while True: 
+            match (str.lower(input("Enable: "))):
+
+                case "yes"|"y":
+                    stationControl.append(True)
+                    print()
+
+                    #test for number input
+                    while True:
+                        try: #enterd a number
+                            stationControl.append(int(input("Search Radius in NM: ")))
+                            break
+                        except: #did not enter a number
+                            print("Must be a number")
+                            print()
+
+                    for op in setUpPrintStore: #loop through setup queston with print then store
+                        done = False
+                        for idx, x in enumerate(setUpQuestions):#loop through setup queston metar, taf, notam
+
+                                while True:
+                                    print()
+                                    match str.lower(input(op + " " + setUpQuestions[idx] + ": ")):
+
+                                        case "all":
+                                            for _ in range(2):
+                                                stationProducts[f"station{op}"].append(True)
+
+                                            done = True
+                                            break
+
+                                        case "yes"|"y":
+                                            stationProducts[f"station{op}"].append(True)
+                                            break
+                                            
+                                        case "n"|"no":
+                                            stationProducts[f"station{op}"].append(False)
+                                            break
+
+                                        case _:
+                                            print("Invalid Entry Reply With [y/n]")
+                                            print()
+                            
+                                if done == True: break
+                                    
+
+                    break #exit enable station loop
+                            
+                case "n"|"no":
+                    for _ in range(3):
+                        stationProducts[f"stationPrint"].append(False)
+                        stationProducts[f"stationSave"].append(False)
+                        stationControl.append(False)
+                    
+                    stationControl[3] = 0
+                    break
+                
+                case "all":
+                    for _ in range(3):
+                        stationProducts[f"stationPrint"].append(False)
+                        stationProducts[f"stationSave"].append(False)
+                        stationControl.append(False)
+                
+                    #test for number input
+                    while True:
+                        try: #enterd a number
+                            stationControl.append(int(input("Search Radius in NM: ")))
+                            break
+                        except: #did not enter a number
+                            print("Must be a number")
+                            print()
+                    break 
+
+                case _:
+                    print("Invalid Entry Reply With [y/n]")
+                    print()
         
-        #begin set up and write changes to jason
-        else:
-            print()
-            setUpQuestions = ["METAR","TAF","NOTAM"]
-            setUpPrintStore = ["Print","Store"]
-            
-            #enable staion
-            while True: 
-                match (str.lower(input("Enable: "))):
-
-                    case "yes"|"y":
-                        newStationJSON["use"]["useStation"] = True
-    
-                        for op in setUpPrintStore: #loop through setup queston with print then store
-                            done = False
-                            for idx, x in enumerate(setUpQuestions):#loop through setup queston metar, taf, notam
-
-                                    while True:
-                                        print("control: " + str.lower(x))
-                                        match str.lower(input(op + " " + setUpQuestions[idx] + ": ")):
-
-                                            case "all":
-                                                newStationJSON["use"][str.lower(op)]["metar"] = True
-                                                newStationJSON["use"][str.lower(op)]["taf"] = True
-                                                newStationJSON["use"][str.lower(op)]["notam"] = True
-                                                done = True
-                                                break
-
-                                            case "yes"|"y":
-                                                newStationJSON["use"][str.lower(op)][str.lower(x)] = True
-                                                break
-                                                
-                                            case "n"|"no":
-                                                newStationJSON["use"][str.lower(op)][str.lower(x)] = False
-                                                break
-
-                                            case _:
-                                                print("Invalid Entry Reply With [y/n]")
-                                                print()
-                                
-                                    if done == True: break
-                                        
-
-                        break #exit enable station loop
-                                
-                    case "n"|"no":
-                        newStationJSON["use"]["useStation"] = False
-                        break
-
-                    case _:
-                        print("Invalid Entry Reply With [y/n]")
-                        print()
-            
-                break
-            break
-    #begin write
-    newStationEntry = {
-        str(newStationData[int(desiredNewStaion)]["properties"]["groupIdentifier"]): {}
-    } #set up an object with the station ID as key
-
-    try:
-        with open(localDatabaseFile, "r+") as f: 
-            localDatabase = json.load(f)
-            localDatabase["stationData"].update(newStationEntry) #add the staion key
-            localDatabase["stationData"][newStationData[int(desiredNewStaion)]["properties"]["groupIdentifier"]].update(newStationJSON) #add station data to new key
-            f.seek(0) #curser to start
-            json.dump(localDatabase, f, indent = 4) #add back
-            f.close()
-        print()
-        print("New Station Added")
-    except:
-
-        print()
-        print("DB WRITE ERR OCCURED")
-        print()
-            
-            
-    
+     
+        asyncio.run(database.add_new_station(stationControl, stationProducts["stationPrint"], stationProducts["stationSave"], newStationData))
+        break
+   
+clearConsole()
 add_to_database()
